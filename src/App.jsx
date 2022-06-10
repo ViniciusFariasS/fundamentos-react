7ec389d01b312c7eb94452d84bc07281201d88e6
Vinicius Farias Silva <vfarias51@gmail.com>
2022-06-10 17:44:48 -0300

    progresso até a aula 28

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,40 @@ import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
 import Aleatorio from "./components/basicos/Aleatorio";
 import Card from "./components/layout/Card";
+import Familia from "./components/basicos/Familia";
+import FamiliaMembro from "./components/basicos/FamiliaMembro";
+import ListaAlunos from "./components/repeticao/ListaAlunos";
+
+import "./App.css";
 
 //_ serve para dizer que a função terá apenas um parametro
 export default () => (
-    <div>
+    <div className="App">
         <h1>Fundamentos React </h1>
-        <Card titulo="Desafio Aleatório">
-            <Aleatorio min={1} max={60}/>
-            <Aleatorio min={1} max={60}/>
-            <Aleatorio min={1} max={60}/>
-            <Aleatorio min={1} max={60}/>
-            <Aleatorio min={1} max={60}/>
-            <Aleatorio min={1} max={60}/>
-        </Card>
+        <div className="Cards">
 
-        <Fragmento />
-        <ComParametro titulo="Segundo componente" aluno="João Silva" nota={5.7} />
-        <Primeiro></Primeiro>
+            <Card titulo="#06 - Repetição" color="#FF5534">
+                <ListaAlunos></ListaAlunos>
+            </Card>
+            <Card titulo="#05 - Componente com filhos" color="#00C58D">
+                <Familia sobrenome="Ferreira">
+                    <FamiliaMembro nome="Pedro"></FamiliaMembro>
+                    <FamiliaMembro nome="Ana"></FamiliaMembro>
+                    <FamiliaMembro nome="Gustavo"></FamiliaMembro>
+                </Familia>
+            </Card>
+            <Card titulo="#04 - Desafio Aleatório" color="#080">
+                <Aleatorio min={1} max={60} />
+            </Card>
+            <Card titulo="#03 - Fragmento" color="#E94C6F">
+                <Fragmento />
+            </Card>
+            <Card titulo="#02 - Com parâmetro" color="#E8B71A">
+                <ComParametro titulo="Segundo componente" aluno="João Silva" nota={5.7} />
+            </Card>
+            <Card titulo="#01 - Primeiro Componente" color="#588C73">
+                <Primeiro></Primeiro>
+            </Card>
+        </div>
     </div>
 );
---
+++ src/data/alunos.js
@@ -0,0 +1,11 @@
+export default [
+    {id: 1, nome: 'Ana', nota: 9.2},
+    {id: 2, nome: 'Bia', nota: 3.2},
+    {id: 3, nome: 'Paulo', nota: 4.2},
+    {id: 4, nome: 'Duda', nota: 5.6},
+    {id: 5, nome: 'André', nota: 9.2},
+    {id: 6, nome: 'Anahi', nota: 2.2},
+    {id: 7, nome: 'Gabi', nota: 4.6},
+    {id: 8, nome: 'José', nota: 8.1},
+    {id: 9, nome: 'Paulo', nota: 1.2},
+]
